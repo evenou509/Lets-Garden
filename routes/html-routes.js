6a38f00bd32abc4b3db6cd0c279cdd6311fc25d5
Jean Fils-Aime <jeaneddy25@gmail.com>
2019-02-24 07:11:55 -0500

    mnp

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,9 +7,9 @@ module.exports = function(app) {
        res.sendFile(path.join(__dirname, "../public/index.html")); 
     })
 
-    // app.get("/newpatient", function(req, res){
-    //    res.sendFile(path.join(__dirname, "../public/new_patient.html")); 
-    // })
+    app.get("/register", function(req, res){
+       res.sendFile(path.join(__dirname, "../public/register.html")); 
+    })
 
     // app.get("/patientview/:id", function(req, res){
     //     res.sendFile(path.join(__dirname, "../public/patient_view.html")); 
