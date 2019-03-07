9f19590e62b845ea216ce3063cee21a8b9b65801
Jean Fils-Aime <jeaneddy25@gmail.com>
2019-03-06 19:23:08 -0500

    remote

--- public/js/search.js
+++ public/js/search.js
@@ -65,36 +65,9 @@ $(document).ready(function(){
         plantCard.append("<h3 id='card-title' class='card-title'>" + plants.name + "</h3>")
         plantCard.append("<p class='card-text'>" + plants.description + "</p>")
         plantCard.append("<p class='card-text'> Stay away from: " + plants.pests + "</p>")
-        plantCard.append("<span> <button class='add btn btn-outline-dark'> Add to you Garden </button>  <button class='swap btn btn-outline-dark'> Request Swap </button> </span>")
+        plantCard.append("<span> <button class='add btn btn-outline-dark' id='add'> Add to you Garden </button>  <button class='swap btn btn-outline-dark'> Request Swap </button> </span>")
 
         $("#print").append(plantCard)
-//  <div>
-//  <div className="card">
-//   <div className="card-body">
-//   <h3 id="card-title" className="card-title">{props.name}</h3>
-//   <br/>
-//   <p className="card-text">{props.description}</p> 
-//   <p className="card-text">{props.watering}</p> 
-//   <p className="card-text">Stay away from {props.pests}</p>
-//   <span >
-//  <button className="add btn btn-outline-dark" {...props} > Add to your Garden</button>
-//  <br/>
-//  <br/>
-//  <button className="swap btn btn-outline-dark" {...props} > Request Swap</button>
-// </span>
-//   </div>
-// </div>
-// </div>
 
-    // var visitDate = result[i].createdAt;
-    // var formatDate = moment(visitDate).format('ll')
-
-    // var history = $("<tr>");
-    // history.append("<td>" + formatDate + "</td>")
-    // history.append("<td>" + result[i].reason_for_visit + "</td>")
-    // history.append("<td>" + result[i].body + "</td>")
-    // history.append('<td>' + result[i].employee + '</td>')
-
-    // $("#print-history").append(history)
  }
 
